f1b7e296caff50f8d7464dc76956ccdd9b94049a
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-03-20 18:42:37 +0700

    Выполняем действие с элементом страницы, click

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -12,7 +12,8 @@ public class FirstTest extends WebDriverSettings{
 
         Assert.assertEquals("Публикации / Моя лента / Хабр", driver.getTitle()); // Проверка соответствия значения и title
 
-        WebElement input = driver.findElement(By.xpath("//button [@class='tm-header__button tm-header__button_burger']")); //Находим кнопку войти на странице habr.com
+        WebElement input = driver.findElement(By.xpath("//button [@class='tm-header-user-menu__login btn btn_solid btn_small']")); //Находим кнопку войти на странице habr.com
+        input.click();  //Нажимаем на элемент расположены по адресу xpath
     }
 
     @Test
@@ -23,6 +24,7 @@ public class FirstTest extends WebDriverSettings{
         Assert.assertEquals("Mail.ru: почта, поиск, новости, прогноз погоды, гороскоп, программа передач", driver.getTitle()); // Проверка соответствия значения и title
 
         WebElement input = driver.findElement(By.xpath("//button[@class='resplash-btn resplash-btn_primary resplash-btn_mailbox-big qopmkfo__b85yax']")); //Находим кнопку войти на странице mail.ru
+        input.click();  //Нажимаем на элемент расположены по адресу xpath
     }
 
 }
