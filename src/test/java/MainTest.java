fe839db6782bcfe42a1baa7820f139d862f8bf2c
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-03-20 17:30:35 +0700

    Метод get класса ChromeDriver

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MainTest {
     public static void main(String[] args) {
-
+    FirstTest test1 = new FirstTest();
+    test1.firstTest();
     }
 }
