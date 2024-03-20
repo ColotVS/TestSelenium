1588cd966b21cdfb57fe0ea6f929cc637c1b6b14
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-03-20 18:03:29 +0700

    Объявление и инициализация веб-драйвера в отдельном классе

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -4,15 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FirstTest {
-    public ChromeDriver driver;
-    static int i =0;
-    @Before                     //Выполняется для всех тестов перед их запуском
-    public void startTest(){
-        driver = new ChromeDriver();
-        i++;
-        System.out.println("Start test:" + i);
-    }
+public class FirstTest extends WebDriverSettings{
+
     @Test
     public void firstTest(){
 
@@ -29,9 +22,5 @@ public class FirstTest {
         Assert.assertEquals("Mail.ru: почта, поиск, новости, прогноз погоды, гороскоп, программа передач", driver.getTitle()); // Проверка соответствия значения и title
     }
 
-    @After                      //Выполняется для всех тестов после их завершения
-    public void close(){
-        driver.quit();  //закрывает страницу
-        System.out.println("Stop test:" + i);
-    }
+
 }
---
+++ src/test/java/WebDriverSettings.java
@@ -0,0 +1,20 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverSettings {
+    public ChromeDriver driver;
+    static int i =0;
+    @Before                     //Выполняется для всех тестов перед их запуском
+    public void startTest(){
+        driver = new ChromeDriver();
+        i++;
+        System.out.println("Start test:" + i);
+    }
+
+    @After                      //Выполняется для всех тестов после их завершения
+    public void close(){
+        driver.quit();  //закрывает страницу
+        System.out.println("Stop test:" + i);
+    }
+}
