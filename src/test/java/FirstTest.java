04c5a21e750169eb3497033577003c072e2b724d
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-03-20 17:55:41 +0700

    JUnit, аннотация @After

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -5,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
     public ChromeDriver driver;
-    @Before                     //общее для всех тестов перед их запуском
+    @Before                     //Выполняется для всех тестов перед их запуском
     public void startTest(){
         driver = new ChromeDriver();
     }
@@ -15,8 +16,6 @@ public class FirstTest {
         driver.get("https://habr.com/ru/feed/");    // открывает страницу
 
         Assert.assertEquals("Публикации / Моя лента / Хабр", driver.getTitle()); // Проверка соответствия значения и title
-
-        driver.quit();  //закрывает страницу
     }
 
     @Test
@@ -25,7 +24,10 @@ public class FirstTest {
         driver.get("https://mail.ru");    // открывает страницу
 
         Assert.assertEquals("Mail.ru: почта, поиск, новости, прогноз погоды, гороскоп, программа передач", driver.getTitle()); // Проверка соответствия значения и title
+    }
 
+    @After                      //Выполняется для всех тестов после их завершения
+    public void close(){
         driver.quit();  //закрывает страницу
     }
 }
