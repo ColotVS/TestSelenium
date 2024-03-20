7ed617b87eca432d39b67244823bee2f5c5072d6
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-03-20 18:06:08 +0700

    Объявление и инициализация веб-драйвера в отдельном классе

--- src/test/java/MainTest.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class MainTest {
-    public static void main(String[] args) {
-        FirstTest test1 = new FirstTest();
-        test1.firstTest();
-    }
-}
